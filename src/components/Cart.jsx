f09f19614eee6bf2cb12f5d4b8b3b3c758ab7fba
jongithub59 <jdelrealj01@gmail.com>
2026-02-05 12:52:23 -0600

    feat: display total price of current cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,15 +4,21 @@ import { Link, useOutlet, useOutletContext } from "react-router";
 function Cart() {
   const { cart, setCart, removeFromCart } = useOutletContext();
 
-  console.log(`Current cart: ${cart}`);
+  const totalCost = cart.reduce((total, item) => total + item.cost, 0); // calculate sum of total cart price
+  const formattedCost = totalCost.toLocaleString(); // format cast to add comma
+
+  console.log(`Current cart: ${cart.map((item) => item.name)}`);
   return (
     <div className="app">
       <div className="cart-page">
         <div className="cart-header">
           <h2>Cart</h2>
+          {/* conditional elements if cart is empty or not */}
+          {cart.length == 0 && <h3> Cart is empty</h3>}
+          {cart.length > 0 && <h3>Total Cost: {formattedCost} Souls</h3>}
           <div className="exit-button">
             <Link to="/">
-              <button>X</button>
+              <button>Go Back</button>
             </Link>
           </div>
         </div>
