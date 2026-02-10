0ca07ad86014e198e3725eb6e30c20ddd97cb1f0
jongithub59 <jdelrealj01@gmail.com>
2026-02-10 11:58:43 -0600

    feat: add navbar to navigate to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router";
+import { Link } from "react-router";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -17,6 +18,23 @@ function App() {
   return (
     <>
       <div className="app">
+        <div className="navbar">
+          <div className="home-button">
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+          </div>
+          <div className="shop-button">
+            <Link to="/Shop">
+              <button>Shop</button>
+            </Link>
+          </div>
+          <div className="cart-button">
+            <Link to="Cart">
+              <button>Cart</button>
+            </Link>
+          </div>
+        </div>
         {/* outlet context allows access to this state variable if it's in the route (which is everything in App) */}
         <Outlet context={{ cart, setCart, clearCart, removeFromCart }} />
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,6 @@ import { Link } from "react-router";
 function Home() {
   return (
     <>
-      <div className="cart-button">
-        <Link to="Cart">
-          <button>Cart</button>
-        </Link>
-      </div>
       <div className="home-page">
         <div className="header">
           <h1>Deadlock</h1>
