d301a519896c90693624157a6984219087c33f54
jongithub59 <jdelrealj01@gmail.com>
2026-02-27 18:39:51 -0600

    style: make home page look more finalized

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router";
+import titleCard from "../assets/deadlock_titlecard.png";
+import video from "../assets/menu_streets_loop2-vmake.mp4";
 
 function Home() {
   return (
     <>
       <div className="home-page">
-        <div className="header">
-          <h1>Deadlock</h1>
+        <div className="title-card">
+          <img src={titleCard} alt="Deadlock" />
         </div>
         <div className="home-page-content">
           <Link to="/Shop">
-            <button>Enter Shop</button>
+            <button>Enter The Curiousity Shop</button>
           </Link>
         </div>
       </div>
+      <video autoPlay muted loop id="video-background">
+        <source src={video} type="video/mp4" />
+      </video>
     </>
   );
 }
