b6a3e2020410c4d76fc03281e5bf6549628b77d9
jongithub59 <jdelrealj01@gmail.com>
2026-04-14 16:17:56 -0500

    prep app to deploy to gh pages for hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import routes from "./components/routes.jsx";
 import "./App.css";
 import App from "./App.jsx";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
